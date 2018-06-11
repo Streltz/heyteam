0e2b995ff481588e2a59f26ada737e730b6b53d1
unknown <davsoudry@gmail.com>
2018-06-11 16:29:22 -0500

    Added Components: Convos, AddConvo, EditConvo. Also App.less

---
+++ hey-team/src/Components/Convos.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class CreateConvo extends Component {
+    state = {
+        title: "",
+        content: ""
+    };
+
+    render() {
+        return (
+            pass
+        )
+    }
+}
