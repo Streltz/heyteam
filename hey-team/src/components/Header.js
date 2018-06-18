c2e116e22f53b987126b5dd1bb050813451895d0
Mike Streltsoff <MStreltsoff@gmail.com>
2018-06-18 15:11:37 -0400

    header changes

--- hey-team/src/components/Header.js
+++ hey-team/src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { signOut } from '../actions/userAction';
 
+import './Header.css'
 
 class Header extends React.Component {
   state = { }
@@ -14,8 +15,22 @@ class Header extends React.Component {
   render() {
     return (
       <div>
+ 
+    
+      <div className="headContain">
+        <div className="slack-logo-wrapper">
+          <img className="slacklogo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDQvf1ORt2PMY4S_V1AoFs57ekWOJKkkppVgs7Je8ZLLC6FBlWSQ"/>
+          </div>
+          <div className="heyTeam">
+        <font familty="Courier Header">Hey Team!</font>
+        <div className="signOut">
+        <button>
         <div onClick={() => {this.handleSignOut()}}><u>Sign Out</u></div>
+        </button>
       </div>
+    </div>
+    </div>
+    </div>
     );
   }
 }
@@ -24,5 +39,7 @@ const mapStateToProps = (state) => {
     return {
     } 
 }
+
+
 
 export default connect(mapStateToProps, { signOut })(Header);
