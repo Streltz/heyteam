5d8c562e2845be3d4df875ea7bcc1375a488a8c9
Lo Saephan <teamsky916@gmail.com>
2018-06-21 10:17:59 -0700

    fixing convo reducer

--- hey-team/src/reducers/ConvosReducer.js
+++ hey-team/src/reducers/ConvosReducer.js
@@ -1,28 +1,43 @@
-const ConvosReducer = (state = [], action) => {
+import { SORTING } from '../actions/convoAction';
+
+const initState = {
+    originalConvos: [],
+    convos: []
+}
+
+const ConvosReducer = (state = initState, action) => {
     switch (action.type) {
         case 'LOADING_CONVOS':
             return { ...state, loading:true };
 
         case 'FETCHED_CONVOS':
-            return action.payload;
+            return {...state, originalConvos: action.payload, convos: action.payload};
         
         case 'CONVO_ADDED':
-            return [ ...state, action.payload ];
-        
-        case 'VIEW_CONVO':
-            return { ...state, current: action.payload, loading: false};
+            return {...state, [...originalConvos, action.payload], [...convos, action.payload]};
 
         case 'EDIT_CONVO':
-            return {
-                ...state,
-                convos: state.convos.map(convo => {
-                    if (convo.id === action.payload.id) {
-                    return action.payload;
-                }
-                return convo;
-            }),
-            loading: false
+            // return {
+            //     ...state,
+            //     convos: state.convos.map(convo => {
+            //         if (convo.id === action.payload.id) {
+            //         return action.payload;
+            //     }
+            //     return convo;
+            // }),
+            // loading: false
         };
+        case SORTING:
+            if(action.payload === "All"){
+                const sorted = state.originalConvos;
+                return {...state, convos: sorted}
+            }
+            if(action.payload === "All"){
+                const sorted = state.originalConvos;
+                return {...state, convos: sorted}
+            }
+
+
         default:
             return state;
     }
