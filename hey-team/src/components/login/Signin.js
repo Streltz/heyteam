b090f42c31a7ef8f2629153690e08a30665aa535
Guy Ross <guyallenross@gmail.com>
2018-06-25 12:41:44 -0400

    Signin - fixed signin handling - added form submittion on enter

--- hey-team/src/components/login/Signin.js
+++ hey-team/src/components/login/Signin.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { signIn, clearFormError } from '../../actions/userAction';
 import { Link } from 'react-router-dom';
-import { Card} from 'reactstrap';
+import { Card } from 'reactstrap';
 
 import './styles.css';
 
@@ -12,13 +12,17 @@ class Signin extends React.Component {
     password: ''
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.props.clearFormError();
   }
 
-  handleSignIn = () => {
-    console.log('sigin prpos', this.props);
-    this.props.signIn(this.state, this.props.history);
+  handleSignIn = (event) => {    
+    if(event != undefined)
+    {
+      event.preventDefault();
+      this.props.signIn(this.state, this.props.history);    
+      console.log("ev", event);
+    }
   }
 
   handleOnChange = (event) => {
@@ -32,22 +36,24 @@ class Signin extends React.Component {
       <main className="main-login">
         <Card className="edge-dull">
           <div className="login card-dull">
-            <div class="logo text-left col-md-12">Hey-Bot</div>
-            <div class="text-left col-md-12 heading"> Sign In </div>
-            <div class="text-left card-descriptor col-md-12">with your Hey-Bot Account</div>
-            <br />
-            <div className="form-error">{this.props.user.formError}</div>
-            <input class="col-md-10 form-control" name="email" value={this.state.email}
-              placeholder="Email" onChange={this.handleOnChange} /><br />
-            
-            <input class="col-md-10 form-control" name="password" type="password" value={this.state.value}
-              placeholder="Password" onChange={this.handleOnChange} /><br />
+            <form onSubmit={(e) => {this.handleSignIn(e)}}>
+              <div class="logo text-left col-md-12">Hey-Bot</div>
+              <div class="text-left col-md-12 heading"> Sign In </div>
+              <div class="text-left card-descriptor col-md-12">with your Hey-Bot Account</div>
+              <br />
+              <div className="form-error">{this.props.user.formError}</div>
+              <input class="col-md-10 form-control" name="email" value={this.state.email}
+                placeholder="Email" onChange={this.handleOnChange} /><br />
 
-            <div class="col-md-12">
-              <button class="signin light-blue-btn" onClick={() => { this.handleSignIn() }}>Sign in</button><br />
-            </div>
+              <input class="col-md-10 form-control" name="password" type="password" value={this.state.value}
+                placeholder="Password" onChange={this.handleOnChange} /><br />
 
-            <Link to="/signup" class="blue-link">Don't have an account?</Link>
+              <div class="col-md-12">
+                <button class="signin light-blue-btn" onClick={() => { this.handleSignIn() }}>Sign in</button><br />
+              </div>
+
+              <Link to="/signup" class="blue-link">Don't have an account?</Link>
+            </form>
           </div>
         </Card>
       </main>
