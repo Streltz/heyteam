b95d3ce3a8e6d4376ecfd3ecee7d801626420ccb
Mike Streltsoff <MStreltsoff@gmail.com>
2018-06-27 17:31:14 -0400

    HeyBot Description

--- hey-team/src/components/Landing/Landing.js
+++ hey-team/src/components/Landing/Landing.js
@@ -41,9 +41,29 @@ class Landing extends React.Component {
         </header>
 
         <section className="section-body">
-        {
-          // Content Goes in here!
-        }
+        
+          <p>
+            Say Hello to Hey Team! A SlackBot for all your team's needs. Finally, Project Manager's can send out polls or questions to a team and 
+            receive their responses in an organized and private way.
+          </p> 
+          <p>
+          How it Works:
+          </p>
+          <p>
+          Project Managers can create, separate and private conversations with the members of their team simultaneously.
+          New Conversations are well organized complete with timestamps and titles. 
+          </p>
+          <p>
+          Questions can be sent immediately or scheduled for a future time and date. Hey Team's new question card comes with a full 
+          list of options such as Time and Date, Participants and whether they want have the conversation privately through 
+          Slack's Direct Messaging feature or in a separate Hey Bot Slack channel.
+          </p>
+          <p>
+          Our Conversations feature will organize all your questions and show Managers who has responded, how they responded and when they responded. 
+          Hey Bot also offers the option of having these organized messages sent to the Manager's e-mail as well for safe record keeping.
+          </p>
+          
+        
         </section>
       </main>
     );
