a683f4bd8b8abcbe37220d33679455e22877c8c7
unknown <davsoudry@gmail.com>
2018-07-11 19:56:24 -0500

    cors

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ return modTime;
 if (!token) { console.log('You must specify a token to use this example'); process.exitCode = 1; return; }
 
 app.use(cors({
-  origin: 'heyteam.app',
+  origin: 'https://www.heyteam.app/',
   credentials: true
 }));
 // app.use(cors());
